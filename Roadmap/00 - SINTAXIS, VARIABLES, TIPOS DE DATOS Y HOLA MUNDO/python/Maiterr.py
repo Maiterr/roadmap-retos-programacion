a5c178509151e9e092fdf302b4abb8e902d1fee1
Maiterr <ch25007@ues.edu.sv>
2025-07-14 15:31:13 -0600

    Create Maiterr.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Maiterr.py
@@ -0,0 +1,22 @@
+#COMENTARIO
+#MI lenguaje seleccionado es phyton y la url es https://www.python.org/
+
+#TIPOS DE COMENTARIOS
+# comentario en una linea 
+"""
+esto  es un comentario 
+de varias
+lineas
+"""
+# VARIABLES
+COSNTANTE = 3.1415
+normalita = 0.0
+
+#TIPOS DE VARIABLES
+texto = "cadena de texto" # cadena de texto(string)
+numero_entero = 7 # entero
+numero decimal = 4.0
+booleano = False 
+booleano = True 
+
+print("Hola mi lenguaje seleccionado es Phyton")
